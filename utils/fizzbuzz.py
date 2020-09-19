5abbfacc63deccd5812819a0e93ef4f6acd9f221
Luis Palacios <lrpalaciosdev@gmail.com>
2020-09-19 12:48:28 -0600

    add linting configuration

--- utils/fizzbuzz.py
+++ utils/fizzbuzz.py
@@ -1,16 +1,17 @@
 def fizz_or_buzz(number):
     if number % 3 == 0 and number % 5 == 0:
-        return 'fizzbuzz'
+        return "fizzbuzz"
     if number % 3 == 0:
-        return 'fizz'
+        return "fizz"
     if number % 5 == 0:
-        return 'buzz'
+        return "buzz"
     return number
+
 
 def fizzbuzz_sequence(limit):
     if limit <= 0:
-        raise ValueError(f'Limit must be greater than 0, but {limit} was passed')
+        raise ValueError(f"Limit must be greater than 0, but {limit} was passed")
     result = []
     for i in range(1, limit + 1):
-        result.append(fizz_or_buzz(i)) 
+        result.append(fizz_or_buzz(i))
     return result
